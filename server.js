97ccc38a3a42482848c4336f382103b118bd8d38
Jacqueline <jskye429@gmail.com>
2020-02-21 18:31:15 -0600

    added some server request functionality in get requests

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var JSONstuff = require("./db/db.json");
 
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -13,14 +13,22 @@ app.use(express.static("public"));
 app.use(express.json());
 
 
-// app.get
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+  
 
 // app.post
 
 
 // JSONstuff.push
 
+
+//app.del
 
 
 
